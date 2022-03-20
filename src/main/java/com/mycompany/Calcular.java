30af7eeacf249989a30833f9beeda874a6c31f09
Victoria Santos <victoriaefs@outlook.com>
2022-03-20 19:56:16 -0300

    first commit

---
+++ src/main/java/com/mycompany/Calcular.java
@@ -0,0 +1,24 @@
+
+package com.mycompany;
+
+/**
+ *
+ * @author Victoria Santos <victoriaefs@outlook.com>
+ */
+public class Calcular {
+    public double soma(double num1, double num2){
+       return num1+num2;
+    }
+    
+    public double subtracao (double num1, double num2){
+       return num1-num2;
+    }
+    
+    public double divisao (double num1, double num2){
+       return num1/num2;
+    }
+    
+    public double multiplicacao(double num1, double num2){
+       return num1*num2;
+    }
+}
